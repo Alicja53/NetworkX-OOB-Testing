c0e5674940befdbb74de3b83c786a1a100441d14
MATRIii <sokolowskik1@o2.pl>
2026-04-10 01:17:49 +0200

    Test obciążeniowy(MATRIii) :test edge flooding dla 500k relacji, , menu
    Pomiar wydajności zapisu 50 relacji na każdy z 10 000 węzłów. Weryfikacja stabilności struktury grafu przy gwałtownym przyroście krawędzi. Poprawa czytelności kodu. Dodanie menu wyboru testu dale prostszego zarządzania przebiegiem testów.

--- tests/test_load_networkx.py
+++ tests/test_load_networkx.py
@@ -6,24 +6,49 @@ class LoadNode:
     """Węzeł zaprojektowany do obciążania pamięci RAM."""
     def __init__(self, id):
         self.id = id
-        # Każdy węzeł niesie ze sobą 5KB 'śmieciowych' danych
-        self.data_payload = "X" * 5120 
+        self.data_payload = "X" * 5120  # 5KB danych
 
     def __hash__(self): return hash(self.id)
     def __eq__(self, other): return self.id == other.id
 
 def test_data_load_capacity():
-    print("\n--- TEST: WYDAJNOŚĆ ZAPISU WŁASNYCH WĘZŁÓW ---")
+    print("\n--- TEST 1: WYDAJNOŚĆ ZAPISU WĘZŁÓW ---")
     G = nx.Graph()
     start = time.time()
-    
-    # Próba masowego zapisu 50 000 ciężkich węzłów
     for i in range(50000):
         G.add_node(LoadNode(i))
-        
     end = time.time()
-    print(f"Dodano 50k ciężkich węzłów w czasie: {end-start:.4f}s")
-    print(f"Przybliżony rozmiar grafu w RAM: {sys.getsizeof(G) / 1024:.2f} KB")
+    print(f"Dodano 50k ciężkich węzłów w: {end-start:.4f}s")
+    print(f"Rozmiar struktury G: {sys.getsizeof(G) / 1024:.2f} KB")
+
+def test_edge_flooding():
+    print("\n--- TEST 2: OBCIĄŻENIE RELACJI (EDGE FLOODING) ---")
+    G = nx.Graph()
+    nodes = [LoadNode(i) for i in range(10000)]
+    G.add_nodes_from(nodes)
+    
+    start = time.time()
+    for i in range(len(nodes) - 1):
+        for j in range(1, 51):
+            if i + j < len(nodes):
+                G.add_edge(nodes[i], nodes[i+j])
+    end = time.time()
+    print(f"Wygenerowano {G.number_of_edges()} krawędzi w: {end-start:.4f}s")
 
 if __name__ == "__main__":
-    test_data_load_capacity()
+    print("WYBÓR TESTU OBCIĄŻENIOWEGO:")
+    print("1. Test węzłów (Memory Load)")
+    print("2. Test krawędzi (Edge Flooding)")
+    print("3. Wykonaj wszystkie (Memory Load + Edge Flooding)")
+    
+    wybor = input("Wybierz numer (1-3): ")
+    
+    if wybor == '1':
+        test_data_load_capacity()
+    elif wybor == '2':
+        test_edge_flooding()
+    elif wybor == '3':
+        test_data_load_capacity()
+        test_edge_flooding()
+    else:
+        print("Błędny wybór. Zamykanie.")
