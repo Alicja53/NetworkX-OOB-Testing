c1696883022f06e90fa813ebbc28b5d3338c150c
Alicja <124045087+Alicja53@users.noreply.github.com>
2026-04-23 18:36:43 +0200

    Implement test for finding shortest path function
    Added a test function for shortest path finding.

--- tests/shortest_path_finder.py
+++ tests/shortest_path_finder.py
@@ -1,13 +1,11 @@
 #import biblioteki do testowania(networkx)
 import networkx as nx
 
-
 # znajdowanie najkrótszych dróg za pomocą shortest_path
 def finding_shortest_path(First_Graph, Second_Graph):
     # zapisywanie wyniku funkcji shortest_patch do zmiennych
     First = nx.shortest_path(First_Graph, 3, 6)
     Second = nx.shortest_path(Second_Graph, 3, 6, weight='weight')
-
 
     # sprawdzanie poprawności wyników funkcji
     if First != [3, 1, 6]:
@@ -18,12 +16,10 @@ def finding_shortest_path(First_Graph, Second_Graph):
         return False
     return True
 
-
 #tworzenie pierwszego grafu testowego bez atrybutu weight
 Edges1=[(1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 3), (3, 4), (4, 5), (5, 6), (6,2)]
 First_Graph = nx.Graph()
 First_Graph.add_edges_from(Edges1)
-
 
 #tworzenie drugiego grafu testowego z atrybutem weight
 Second_Graph = nx.Graph()
@@ -38,13 +34,6 @@ Second_Graph.add_edge(4, 5, weight=1)
 Second_Graph.add_edge(5, 6, weight=1)
 Second_Graph.add_edge(6, 2, weight=3)
 
-
-#rysowanie obu grafów testowych z labelami
-nx.draw(First_Graph, with_labels=True)
-nx.draw(Second_Graph, with_labels=True)
-
-
-print(finding_shortest_path(First_Graph, Second_Graph))
-
-
-
+# Funkcja testowa dla pytest (konieczna do automatyzacji w GitHub Actions)
+def test_shortest_path():
+    assert finding_shortest_path(First_Graph, Second_Graph) == True
